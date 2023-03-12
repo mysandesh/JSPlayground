30a40c870baab369c6cc51444044bdc6be130963
Sandesh Acharya <mysandesh@gmail.com>
2023-03-12 22:14:28 +0545

    Added Destructuring Arrays

---
+++ 103-destructuring-arrays.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const restaurant = {
+  name: "Classio Italino",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegeterian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+const arr = [2, 3, 4];
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+const [first, , second] = restaurant.categories;
+console.log(first, second);
+
+//Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
